cc65ee4e602d661fcdcc8547e7ee9e9068b2163b
Sadra <msmohamadi1385@gmail.com>
2025-05-03 15:01:06 +0330

    made time algorithm

--- src/main/java/com/project/Models/Time/Time.java
+++ src/main/java/com/project/Models/Time/Time.java
@@ -3,9 +3,9 @@ package com.project.Models.Time;
 import com.project.Models.Enums.Seasons;
 
 public class Time {
-    private Seasons currentSeason;
-    private int spentDays;
-    private int currentHour;
+    private Seasons currentSeason = Seasons.Spring;
+    private int spentDays = 0;
+    private int currentHour = 9;
 
     public Seasons getCurrentSeason() {
         return currentSeason;
@@ -24,7 +24,31 @@ public class Time {
     }
 
     public void changeTime(int hour) {
+        this.currentHour = hour;
+        if (this.currentHour >= 22) {
+            this.currentHour = 9;
+            this.spentDays ++;
+            if (this.spentDays >= 28) {
+                switch (this.currentSeason) {
+                    case Spring:
+                        this.currentSeason = Seasons.Summer;
+                        break;
 
+                    case Summer:
+                        this.currentSeason = Seasons.Fall;
+                        break;
+
+                    case Fall:
+                        this.currentSeason = Seasons.Winter;
+                        break;
+
+                    case Winter:
+                        this.currentSeason = Seasons.Spring;
+                        break;
+                }
+                spentDays = 0;
+            }
+        }
     }
     private final int LengthOfSeason = 28;
 }
