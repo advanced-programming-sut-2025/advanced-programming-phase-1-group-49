c6ac7ae7d921821912d559652704dbd729d0f7ac
arumakan <arumakanGIT@gmail.com>
2025-05-05 19:10:57 +0330

    start login menu

--- src/main/java/com/project/Models/Enums/LoginMenuCommands.java
+++ src/main/java/com/project/Models/Enums/LoginMenuCommands.java
@@ -1,12 +1,20 @@
 package com.project.Models.Enums;
 
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public enum LoginMenuCommands implements Commands {
-    ;
+    login("^\\s*login\\s+-u\\s+(?<username>\\S*)\\s+-p\\s+(?<password>\\S*)" +
+          "((\\s+-stay logged in)?)\\s*$");
+
+    private final String pattern;
+
+    LoginMenuCommands(String pattern) {
+        this.pattern = pattern;
+    }
 
     @Override
     public Matcher getMatcher(String input) {
-        return null;
+        return Pattern.compile(pattern).matcher(input);
     }
 }
--- src/main/java/com/project/view/LoginMenu.java
+++ src/main/java/com/project/view/LoginMenu.java
@@ -1,16 +1,29 @@
 package com.project.view;
 
 import com.project.Controllers.LoginMenuController;
+import com.project.Models.Enums.LoginMenuCommands;
 
 import java.util.Scanner;
 import java.util.regex.Matcher;
 
 public class LoginMenu implements AppMenu {
     LoginMenuController controller = new LoginMenuController();
+    private boolean printMenuInfo = true;
 
     @Override
     public void check(Scanner scanner) {
+        if (printMenuInfo) {
+            System.out.println("""
+                    
+                    """);
+        }
+
         String command = scanner.nextLine();
         Matcher matcher;
+
+        if ((matcher = LoginMenuCommands.login.getMatcher(command)).find())
+            System.out.println(controller.Login(matcher.group(1), matcher.group(2)));
+        else
+            System.out.println("Invalid command");
     }
 }
