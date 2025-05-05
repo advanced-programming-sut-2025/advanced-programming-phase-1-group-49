33f8c955f81a59e166c3b654a406b09fc4437374
Sadra <msmohamadi1385@gmail.com>
2025-05-05 16:36:20 +0330

    Don't remember

---
+++ src/main/java/com/project/Models/Item/Artisan.java
@@ -0,0 +1,4 @@
+package com.project.Models.Item;
+
+public class Artisan {
+}
--- src/main/java/com/project/Models/LivingBeings/Animal.java
+++ src/main/java/com/project/Models/LivingBeings/Animal.java
@@ -1,5 +1,12 @@
 package com.project.Models.LivingBeings;
 
+import com.project.Models.Item.Item;
+
+import java.util.ArrayList;
+
 public abstract class Animal implements LivingBeings {
+    protected int cost;
+    ArrayList<Item> products;
+    
 }
 
