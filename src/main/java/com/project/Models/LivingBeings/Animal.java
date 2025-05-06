8a8341f58bafc5f3237e97606c0987af79705dd1
arumakan <arumakanGIT@gmail.com>
2025-05-06 14:48:07 +0330

    complete adding Observe Time

--- src/main/java/com/project/Models/LivingBeings/Animal.java
+++ src/main/java/com/project/Models/LivingBeings/Animal.java
@@ -1,6 +1,9 @@
 package com.project.Models.LivingBeings;
 
+import com.project.Models.Item.Item;
 import com.project.Models.Time.ObserveTime;
+
+import java.util.ArrayList;
 
 public abstract class Animal implements LivingBeings, ObserveTime {
     protected int cost;
