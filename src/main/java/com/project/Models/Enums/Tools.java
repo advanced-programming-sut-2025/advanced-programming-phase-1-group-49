9b906b02027736a12ae433d077b232518e163189
Sadra <msmohamadi1385@gmail.com>
2025-05-08 22:42:30 +0330

    tools

--- src/main/java/com/project/Models/Enums/Tools.java
+++ src/main/java/com/project/Models/Enums/Tools.java
@@ -3,14 +3,14 @@ package com.project.Models.Enums;
 import com.project.Models.Tools.Tool;
 
 public enum Tools {
-    Axe(new Tool(ToolLevel.basic)),
-    FishingPole(new Tool(ToolLevel.basic)),
-    Hoe(new Tool(ToolLevel.basic)),
-    MilkPail(new Tool(ToolLevel.basic)),
-    Pickaxe(new Tool(ToolLevel.basic)),
-    Scythe(new Tool(ToolLevel.basic)),
-    Shear(new Tool(ToolLevel.basic)),
-    WateringCan(new Tool(ToolLevel.basic));
+    Axe(new Tool().setName("Axe").setEnergy(5).setCapacity(1)),
+    FishingPole(new Tool()),
+    Hoe(new Tool()),
+    MilkPail(new Tool()),
+    Pickaxe(new Tool()),
+    Scythe(new Tool()),
+    Shear(new Tool()),
+    WateringCan(new Tool());
 
     private final Tool tool;
 
--- src/main/java/com/project/Models/Tools/Tool.java
+++ src/main/java/com/project/Models/Tools/Tool.java
@@ -3,16 +3,180 @@ package com.project.Models.Tools;
 import com.project.Models.Enums.ToolLevel;
 
 public class Tool {
-    ToolLevel level;
+    String owner;
+    String name;
+    int energy;
+    int capacity = 0;
+
     boolean digging = false;
     boolean breakStone = false;
     boolean undig = false;
     boolean deleteItem = false;
+    boolean cutTree = false;
+    boolean deleteBranches = false;
+    boolean watering = false;
+    boolean takeWater = false;
+    boolean fishing = false;
+    boolean cutWeed = false;
+    boolean reap = false;
+    boolean milking = false;
+    boolean shearing = false;
 
+    public String getOwner() {
+        return owner;
+    }
 
+    public Tool setOwner(String owner) {
+        this.owner = owner;
+        return this;
+    }
 
-    public Tool(ToolLevel level) {
-        this.level = level;
+    public String getName() {
+        return name;
+    }
+
+    public Tool setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public Tool setCapacity(int capacity) {
+        this.capacity = capacity;
+        return this;
+    }
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    public Tool setEnergy(int energy) {
+        this.energy = energy;
+        return this;
+    }
+
+    public boolean isDigging() {
+        return digging;
+    }
+
+    public Tool setDigging(boolean digging) {
+        this.digging = digging;
+        return this;
+    }
+
+    public boolean isBreakStone() {
+        return breakStone;
+    }
+
+    public Tool setBreakStone(boolean breakStone) {
+        this.breakStone = breakStone;
+        return this;
+    }
+
+    public boolean isUndig() {
+        return undig;
+    }
+
+    public Tool setUndig(boolean undig) {
+        this.undig = undig;
+        return this;
+    }
+
+    public boolean isCutTree() {
+        return cutTree;
+    }
+
+    public Tool setCutTree(boolean cutTree) {
+        this.cutTree = cutTree;
+        return this;
+    }
+
+    public boolean isDeleteBranches() {
+        return deleteBranches;
+    }
+
+    public Tool setDeleteBranches(boolean deleteBranches) {
+        this.deleteBranches = deleteBranches;
+        return this;
+    }
+
+    public boolean isDeleteItem() {
+        return deleteItem;
+    }
+
+    public Tool setDeleteItem(boolean deleteItem) {
+        this.deleteItem = deleteItem;
+        return this;
+    }
+
+    public boolean isTakeWater() {
+        return takeWater;
+    }
+
+    public Tool setTakeWater(boolean takeWater) {
+        this.takeWater = takeWater;
+        return this;
+    }
+
+    public boolean isWatering() {
+        return watering;
+    }
+
+    public Tool setWatering(boolean watering) {
+        this.watering = watering;
+        return this;
+    }
+
+    public boolean isCutWeed() {
+        return cutWeed;
+    }
+
+    public Tool setCutWeed(boolean cutWeed) {
+        this.cutWeed = cutWeed;
+        return this;
+    }
+
+    public boolean isFishing() {
+        return fishing;
+    }
+
+    public Tool setFishing(boolean fishing) {
+        this.fishing = fishing;
+        return this;
+    }
+
+    public boolean isReap() {
+        return reap;
+    }
+
+    public Tool setReap(boolean reap) {
+        this.reap = reap;
+        return this;
+    }
+
+    public boolean isShearing() {
+        return shearing;
+    }
+
+    public Tool setShearing(boolean shearing) {
+        this.shearing = shearing;
+        return this;
+    }
+
+    public boolean isMilking() {
+        return milking;
+    }
+
+    public Tool setMilking(boolean milking) {
+        this.milking = milking;
+        return this;
+    }
+
+    public Tool() {
+
     }
 
     public void decreaseEnergy() {
