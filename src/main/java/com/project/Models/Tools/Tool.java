1b21e61c06ed0450b0d6772331b60779ecfcdde4
Sadra <msmohamadi1385@gmail.com>
2025-05-08 21:34:00 +0330

    some changes on Tool

--- src/main/java/com/project/Models/Tools/Tool.java
+++ src/main/java/com/project/Models/Tools/Tool.java
@@ -4,6 +4,12 @@ import com.project.Models.Enums.ToolLevel;
 
 public class Tool {
     ToolLevel level;
+    boolean digging = false;
+    boolean breakStone = false;
+    boolean undig = false;
+    boolean deleteItem = false;
+
+
 
     public Tool(ToolLevel level) {
         this.level = level;
