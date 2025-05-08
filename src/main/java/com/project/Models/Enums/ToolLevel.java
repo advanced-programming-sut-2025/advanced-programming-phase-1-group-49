44510959f659b69f808e5c47d48d85888302ae22
Sadra <msmohamadi1385@gmail.com>
2025-05-08 23:34:58 +0330

    tool enum

--- src/main/java/com/project/Models/Enums/ToolLevel.java
+++ src/main/java/com/project/Models/Enums/ToolLevel.java
@@ -1,16 +1,18 @@
 package com.project.Models.Enums;
 
 public enum ToolLevel {
-    basic(5),
-    Cu(4),
-    Fe(3),
-    Au(2),
-    Ir(1);
+    basic(5, 0),
+    Cu(4, 1),
+    Fe(3, 2),
+    Au(2, 3),
+    Ir(1, 4);
 
     private final int energy;
+    private final int level;
 
-    ToolLevel(int energy) {
+    ToolLevel(int energy, int level) {
         this.energy = energy;
+        this.level = level;
     }
 
     public int getEnergy() {
--- src/main/java/com/project/Models/Enums/Tools.java
+++ src/main/java/com/project/Models/Enums/Tools.java
@@ -3,11 +3,11 @@ package com.project.Models.Enums;
 import com.project.Models.Tools.Tool;
 
 public enum Tools {
-    Axe(new Tool().setName("Axe").setEnergy(5).setCapacity(1)),
-    FishingPole(new Tool()),
-    Hoe(new Tool()),
-    MilkPail(new Tool()),
-    Pickaxe(new Tool()),
+    Axe(new Tool().setName("Axe").setEnergy(5).setCapacity(1).setLevel(ToolLevel.basic).setCutTree(true).setDeleteBranches(true)),
+    FishingPole(new Tool().setName("FishingPole").setCapacity(1).setLevel(ToolLevel.basic).setFishing(true)),
+    Hoe(new Tool().setName("Hoe").setCapacity(1).setLevel(ToolLevel.basic).setDigging(true)),
+    MilkPail(new Tool().setName("MilkPail").setCapacity(1).setLevel(ToolLevel.basic).setMilking(true)),
+    Pickaxe(new Tool().setName("Pickaxe").setCapacity(1).setLevel(ToolLevel.basic).setBreakStone(true).setUndig(true).setDeleteItem(true)),
     Scythe(new Tool()),
     Shear(new Tool()),
     WateringCan(new Tool());
