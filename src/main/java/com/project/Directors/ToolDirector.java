091ce13d6ccb181b182f36936a48831a1b8b07cb
Sadra <msmohamadi1385@gmail.com>
2025-05-08 22:42:30 +0330

    tools

---
+++ src/main/java/com/project/Directors/ToolDirector.java
@@ -0,0 +1,11 @@
+package com.project.Directors;
+
+import com.project.Builders.ToolBuilder;
+import com.project.Models.Tools.Tool;
+
+public class ToolDirector extends ToolBuilder {
+    public Tool buildHoe() {
+        Tool tool = new Tool();
+
+    }
+}
